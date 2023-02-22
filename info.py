6731a6372d366b4f76be4e04970fdfcc8f5c7359
multiverseofmadnessh <124427766+multiverseofmadnessh@users.noreply.github.com>
2023-02-22 23:07:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,12 +14,12 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '25514026'))
 API_HASH = environ.get('API_HASH', 'e7fcf97f908583890b80814ef55292a8')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+BOT_TOKEN = environ.get('BOT_TOKEN', "5767710447:AAFCKF--1KepltboyP5wGH2usff6JZUgidw")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/b806ad314d0c415571bde.jpg')).split()
+PICS = (environ.get('PICS', 'https://i.imgur.com/vfeN3bB.jpg')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5656858999').split()]
@@ -34,10 +34,10 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://gobikrish:gobi630554@cluster0.fwyyvy1.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Teleam_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Teleertgram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001895653135'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001853242511'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
